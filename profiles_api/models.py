78b8f895377086c5924cf2566c192ac2222934e1
danielbeleza <danielbeleza@gmail.com>
2021-10-14 17:23:41 +0100

    Add name parameter to create_superuser function

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -21,7 +21,7 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
-    def create_superuser(self, email, password):
+    def create_superuser(self, email, name, password):
         """New super user with given details"""
         user = self.create_user(email, name, password)
 
